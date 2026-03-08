71f076f4e3a840376a130d607362e9a2173ffbba
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-03-08 20:31:51 -0300

    add input for temperature search

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
+from datetime import datetime
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=-10.83&longitude=-63.34&hourly=temperature_2m&forecast_days=1"
+lat = float(input("lat: "))
+lon = float(input("lon: "))
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&forecast_days=1"
 
 response = requests.get(url)
 data = response.json()
 
-print(data)
+# hora atual
+now = datetime.now().strftime("%Y-%m-%dT%H:00")
+
+times = data["hourly"]["time"]
+temps = data["hourly"]["temperature_2m"]
+
+# encontra indice da hora atual
+index = times.index(now)
+
+# temperatura correspondente
+current_temp = temps[index]
+
+print("temp atual:", current_temp, "C")
